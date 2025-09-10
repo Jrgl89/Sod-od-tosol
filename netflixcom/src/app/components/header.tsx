4b3585985ecc4887d09e8da7c748bac66798d6c2
Student <student@Nests-iMac.local>
2025-09-10 14:32:39 +0800

    fist

---
+++ netflixcom/src/app/components/header.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import React from "react";
+
+function Header() {
+  return (
+    <header className="flex justify-between items-center p-6 bg-black fixed w-full top-0 z-10">
+      <a href="/">
+        <img src="logo.png" className="w-36 ml-10" />
+      </a>
+      <nav>
+        <ul className="flex space-x-8 mr-10">
+          <li>
+            <Link href="/" className="hover:text-gray-800">
+              Нүүр хуудас
+            </Link>
+          </li>
+          <li>
+            <Link href="/" className="hover:text-gray-800">
+              Кино сан
+            </Link>
+          </li>
+          <li>
+            <Link href="/" className="hover:text-gray-800">
+              ТВ шоу
+            </Link>
+          </li>
+          <li>
+            <Link href="/" className="hover:text-gray-800">
+              Миний жагсаалт
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
--- netflixcom/src/app/page.tsx
+++ netflixcom/src/app/page.tsx
@@ -1,30 +1,22 @@
 import Image from "next/image";
 import Link from "next/link";
+import Header from "./components/header";
+import { getAllMovies } from "@/services/movieService";
 
-interface MovieType
-{
-  id:number;
-  title:string;
-  backdrop_bath:string;
-  poster_bath:string;
-  overview:string;  
+interface MovieType {
+  id: number;
+  title: string;
+  backdrop_bath: string;
+  poster_bath: string;
+  overview: string;
 }
-export default function Home() {
+export default async function Home() {
+  const movies = await getAllMovies();
+  console.log(movies);
+
   return (
     <div className="bg-black text-white min-h-screen">
-      <header className="flex justify-between items-center p-6 bg-black fixed w-full top-0 z-10">
-        <a href="/">
-        <img src="logo.png" className="w-36 ml-10"/>
-        </a>
-        <nav>
-          <ul className="flex space-x-8 mr-10">
-            <li><Link href="/" className="hover:text-gray-800">Нүүр хуудас</Link></li>
-            <li><Link href="/" className="hover:text-gray-800">Кино сан</Link></li>
-            <li><Link href="/" className="hover:text-gray-800">ТВ шоу</Link></li>
-            <li><Link href="/" className="hover:text-gray-800">Миний жагсаалт</Link></li>  
-          </ul>
-        </nav>
-      </header>
+      <Header />
     </div>
   );
 }
